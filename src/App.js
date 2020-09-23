5e94894bcc3fd53e77b6cd84bc2f5833ced16748
Arnold <arnold.gutib@gmail.com>
2020-09-23 14:48:27 +0800

    Visual Changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "./output-onlinepngtools.png";
+import logo from "./ISI_Logo.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
